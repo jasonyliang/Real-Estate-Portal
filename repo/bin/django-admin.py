34f94f6bae8ec158136282736b649851ace35707
Jason Liang <jjjasonliang@gmail.com>
2018-03-16 10:55:05 -0700

    new project

---
+++ repo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jhajhajhajha1/Desktop/Github/Real-Estate-Portal/repo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
