94c51e3d26b7f824ac98cf112fb5ae369258b39c
Jason Liang <jjjasonliang@gmail.com>
2018-05-29 16:15:04 +0800

    models

---
+++ repo/src/repo/homesapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 200)
+    location_type = models.CharField(max_length = 200)
+
+class Property(models.Model):
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    property_name = models.CharField(max_length = 200)
+    area = models.CharField(max_length = 200)
