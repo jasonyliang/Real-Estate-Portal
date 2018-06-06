9fcb65d402b13c4f311fb4e6ff58969c2d8d0759
Jason Liang <jjjasonliang@gmail.com>
2018-06-06 10:12:23 +0900

    location and property names

--- repo/src/repo/homesapp/models.py
+++ repo/src/repo/homesapp/models.py
@@ -5,8 +5,12 @@ from django.db import models
 class Location(models.Model):
     location_name = models.CharField(max_length = 200)
     location_type = models.CharField(max_length = 200)
-
+    def __str__(self):
+        return self.location_name
 class Property(models.Model):
     location = models.ForeignKey(Location, on_delete = models.CASCADE)
     property_name = models.CharField(max_length = 200)
     area = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.property_name
