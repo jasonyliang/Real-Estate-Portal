42a70586069636f156072189dca6e2b42c9cb5b4
Jason Liang <jjjasonliang@gmail.com>
2018-06-11 22:27:15 +0900

    view

--- repo/src/repo/homesapp/views.py
+++ repo/src/repo/homesapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 from django.views import generic 
+from .models import Location
 # Create your views here.
 
 class IndexView(generic.ListView):
-    template_name = 'homesapp/index.html'
+    template_name = 'index.html'
 
     def get_queryset(self):
         return Location.objects.all()
