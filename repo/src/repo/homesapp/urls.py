73769cb6bedbea03c089577a1e7163d2b715d710
Jason Liang <jjjasonliang@gmail.com>
2018-06-13 10:14:00 +0900

    property model and view

--- repo/src/repo/homesapp/urls.py
+++ repo/src/repo/homesapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from .import views
 urlpatterns = [
-    url(r'^', views.IndexView.as_view(), name = 'index' ),
+    url(r'^$', views.IndexView.as_view(), name = 'index' ),
+    url(r'^(?P<pk>[0-9]+)/$', views.LocationView.as_view(),name='property'),
+    url(r'^([0-9]+)/(?P<pk>[0-9]+)/$', views.PropertyView.as_view(), name  = "propertyview"),
 ]
--- repo/src/repo/homesapp/views.py
+++ repo/src/repo/homesapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.views import generic 
-from .models import Location
+from .models import Location, Property
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -10,3 +10,12 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Location.objects.all()
     
+class LocationView(generic.DetailView):
+    model = Location
+    template_name = "locationview.html"
+
+
+
+class PropertyView(generic.DeleteView):
+    model = Property
+    template_name = "propertyview.html"
